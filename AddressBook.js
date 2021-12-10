559073639349f8b66a3c2c4adb240524ce0e324c
Janani <vsvprema@gmail.com>
2021-12-10 23:00:17 +0530

    Added: Create a Address Book

---
+++ AddressBook.js
@@ -0,0 +1,12 @@
+const PersonDetails = require("./PersonDetails.js");
+const prompt = require('prompt-sync')();
+var person = new PersonDetails();
+person.firstName = prompt("Enter First Name : ");
+person.lastName = prompt("Enter Last Name : ");
+person.address = prompt("Enter Address : ");
+person.city = prompt("Enter City : ");
+person.state = prompt("Enter State : ");
+person.zip = prompt("Enter Zip : ");
+person.phoneNumber = prompt("Enter Phone Number : ");
+person.email = prompt("Enter Email : ");
+console.log(PersonDetails.toString()); 
