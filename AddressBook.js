9acb1179222badd6637322004b99255533dd5f67
Janani <vsvprema@gmail.com>
2021-12-11 22:07:02 +0530

    Added: Created a new contact

--- AddressBook.js
+++ AddressBook.js
@@ -1,16 +1,27 @@
-const PersonDetails = require("./PersonDetails.js");
+//importing another class file to the class file
+const PersonInformation = require("./PersonInformation.js");
 const prompt = require('prompt-sync')();
-var person = new PersonDetails();
-try{
-person.firstName = prompt("Enter First Name : ");
-person.lastName = prompt("Enter Last Name : ");
-person.address = prompt("Enter Address : ");
-person.city = prompt("Enter City : ");
-person.state = prompt("Enter State : ");
-person.zip = prompt("Enter Zip : ");
-person.phoneNumber = prompt("Enter Phone Number : ");
-person.email = prompt("Enter Email : ");
-console.log(PersonDetails.toString()); 
-}catch (exception) {
-    console.error(exception);
-} 
+
+class AddressBook {
+    contactList = new Array();
+    insert(contactList) {
+        let personInfo = new PersonInformation();
+        try {
+            personInfo.firstName = prompt("Enter First Name : ");
+            personInfo.lastName = prompt("Enter Last Name : ");
+            personInfo.address = prompt("Enter Address : ");
+            personInfo.city = prompt("Enter City : ");
+            personInfo.state = prompt("Enter State : ");
+            personInfo.zip = prompt("Enter Zip : ");
+            personInfo.phoneNumber = prompt("Enter Phone Number : ");
+            personInfo.email = prompt("Enter Email : ");
+            contactList.push(personInfo);
+
+            return contactList;
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
+
+module.exports = AddressBook;
--- PersonDetails.js
+++ PersonDetails.js
@@ -1,4 +1,4 @@
-class PersonDetails {
+class PersonInformation {
 
     //property
     firstName;
@@ -10,25 +10,10 @@ class PersonDetails {
     phoneNumber;
     email;
 
-    //constructor
-    constructor(firstName, lastName, address, city, state, zip, phoneNumber, email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.city = city;
-        this.state = state;
-        this.zip = zip;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-    }
-
-    /**
-     * @param {string} firstName
-     */
+    //setter and getter method
     set firstName(firstName) {
-        this.firstName = firstName;
-        let firstnameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-        if (firstnameRegex.test(firstName)) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if (nameRegex.test(firstName)) {
             this.firstName = firstName;
         } else {
             throw "First Name is Incorrect!";
@@ -39,13 +24,9 @@ class PersonDetails {
         return this.firstName;
     }
 
-    /**
-     * @param {string} lastName
-     */
     set lastName(lastName) {
-        this.lastName = lastName;
-        let lastnameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
-        if (lastnameRegex.test(lastName)) {
+        let nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if (nameRegex.test(lastName)) {
             this.lastName = lastName;
         } else {
             throw "Last Name is Incorrect!";
@@ -56,12 +37,9 @@ class PersonDetails {
         return this.lastName;
     }
 
-    /**
-     * @param {string} address
-     */
     set address(address) {
-        let addressRegex = RegExp('^([0-9A-Za-z\\s,-\\\\.]{4,})$');
-        if (addressRegex.test(address)) {
+        let addRegex = RegExp('^([0-9A-Za-z\\s,-\\\\.]{4,})$');
+        if (addRegex.test(address)) {
             this.address = address;
         } else {
             throw "Address is Incorrect!";
@@ -72,9 +50,6 @@ class PersonDetails {
         return this.address;
     }
 
-    /**
-     * @param {any} city
-     */
     set city(city) {
         let cityRegex = RegExp('^([A-Za-z\\s]{4,})$');
         if (cityRegex.test(city)) {
@@ -88,11 +63,7 @@ class PersonDetails {
         return this.city;
     }
 
-    /**
-     * @param {any} state
-     */
     set state(state) {
-        this.state = state;
         let stateRegex = RegExp('^([A-Za-z\\s]{4,})$');
         if (stateRegex.test(state)) {
             this.state = state;
@@ -105,11 +76,7 @@ class PersonDetails {
         return this.state;
     }
 
-    /**
-     * @param {any} zip
-     */
     set zip(zip) {
-        this.zip = zip;
         let zipRegex = RegExp('^([0-9]{6})|([0-9]{3}(\\s)[0-9]{3})$');
         if (zipRegex.test(zip)) {
             this.zip = zip;
@@ -122,11 +89,7 @@ class PersonDetails {
         return this.zip;
     }
 
-    /**
-     * @param {any} phoneNumber
-     */
     set phoneNumber(phoneNumber) {
-        this.phoneNumber = phoneNumber;
         let phoneRegex = RegExp('^(0|91)?[\\s][0-9]{10}$');
         if (phoneRegex.test(phoneNumber)) {
             this.phoneNumber = phoneNumber;
@@ -139,11 +102,7 @@ class PersonDetails {
         return this.phoneNumber;
     }
 
-    /**
-     * @param {any} email
-     */
     set email(email) {
-        this.email = email;
         let emailRegex = RegExp('^[A-Za-z0-9!#$%&*+\\\\=?`{|}~^-]+(?:\\.[A-Za-z!#$%&*+\\\\=?`{|}~^-]+)*@(?:([0-9-]{1}|[a-zA-Z]{3,5})\\.)+[a-zA-Z]{2,3}$');
         if (emailRegex.test(email)) {
             this.email = email;
@@ -158,10 +117,9 @@ class PersonDetails {
 
     //method
     toString() {
-        return "firstName = " + this.firstName + ", lastName = " + this.lastName + ", address = " + this.address + ", city = " + this.city +
-            ", state = " + this.state + ", zip = " + this.zip + ", phoneNumber = " + this.phoneNumber + ", email = " + this.email;
+        return "firstName = " + this.firstName + ", lastName = " + this.lastName + ", address = " + this.address + ", city = " + this.city + ", state = " + this.state + ", zip = " + this.zip + ", phoneNumber = " + this.phoneNumber + ", email = " + this.email;
     }
 }
 
 //exporting the class file to another class
-module.exports = PersonDetails; 
+module.exports = PersonInformation;
